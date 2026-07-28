78653b709c2ae884191f7170fc37e0cde0bb1ac3
wallartscoo-dot <wallarts.coo@gmail.com>
2026-07-28 14:22:40 +0500

    Update upload.js

--- backend/src/routes/upload.js
+++ backend/src/routes/upload.js
@@ -2,7 +2,7 @@ import express from "express";
 import multer from "multer";
 import path from "path";
 import fs from "fs";
-import pdf from "pdf-parse";
+import { PDFParse } from "pdf-parse";
 import mammoth from "mammoth";
 
 console.log("Upload route loaded");
